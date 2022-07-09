be4d750b017e7110a690aaa1d03dd918d2478b56
tranlamm <106047848+tranlamm@users.noreply.github.com>
2022-07-09 12:19:41 +0700

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,13 @@
 package main;
 
-import javax.swing.UIManager;
-
 import view.SignInView;
 
 public class Main  {
 	public static void main(String[] args) {
 		try {
-//			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			new SignInView();
 		} catch (Exception e) {
-			// TODO: handle exceptiona
+			e.printStackTrace();
 		}
 	}
 }
